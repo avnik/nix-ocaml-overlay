6a0c52f3aa53d27a62ee934335ef006bcc08a329
Alexander V. Nikolaev <avn@avnik.info>
2023-10-24 20:12:01 +0300

    WIP: fork from playground repo
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

---
+++ README.md
@@ -0,0 +1,9 @@
+## Nixpkgs+ocaml cross build overlay and playground
+
+Simplified cross-environment for build problematic packages (which don't cross build cleanly)
+
+## How to build/debug 
+
+```sh
+# nix build ".#crossed" -L --cores 16 -j 1
+```
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs";
+        flake-parts = {
+          url = "github:hercules-ci/flake-parts";
+          inputs.nixpkgs-lib.follows = "nixpkgs";
+        };
+    };
+    outputs = { self, flake-parts, ...}@inputs:  flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      flake = { lib, ...}: {
+        nixosConfigurations = {
+          crossed = lib.nixosSystem {
+            system = "aarch64-linux";
+            modules = [
+              {
+                nixpkgs.hostPlatform.system = "aarch64-linux";
+                nixpkgs.buildPlatform.system = "x86_64-linux";
+                nixpkgs.overlays =  [
+                  (import ./ocaml.nix { inherit (inputs) nixpkgs; })
+                  (final: prev: {
+                     caml-crush = final.callPackage ./caml-crush.nix { };
+                  })
+                ];
+              }
+              ({ pkgs, ...}: {
+                environment.systemPackages = with pkgs; [
+                  # List problematic packages here
+                  caml-crush 
+                ];
+                boot.isContainer = true; # Don't build kernel and other slow things
+              })
+            ];
+          };
+        };
+      };
+      perSystem = { self', inputs', system, pkgs, ... }: {
+        packages = {
+         crossed = self.nixosConfigurations.crossed.config.system.build.toplevel;
+        };
+      };
+    };
+}
