e39b226d7c4b2ffc84251d50fdcfb31204c94b05
Alexander V. Nikolaev <avn@avnik.info>
2023-10-24 20:27:04 +0300

    Add utilities
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- flake.nix
+++ flake.nix
@@ -5,9 +5,32 @@
           url = "github:hercules-ci/flake-parts";
           inputs.nixpkgs-lib.follows = "nixpkgs";
         };
+
+    # utilities
+    flake-root.url = "github:srid/flake-root";
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    devour-flake = {
+      url = "github:srid/devour-flake";
+      flake = false;
+    };
     };
     outputs = { self, flake-parts, ...}@inputs:  flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" ];
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.flake-root.flakeModule
+        inputs.treefmt-nix.flakeModule
+        ./checks.nix
+        ./formatter.nix
+      ];
       flake = { lib, ...}: {
         nixosConfigurations = {
           crossed = lib.nixosSystem {
