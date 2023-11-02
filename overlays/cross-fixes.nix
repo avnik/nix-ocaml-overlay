e8c62f2afb910f2910e039a13a2ef6440c9a59a9
Alexander V. Nikolaev <avn@avnik.info>
2023-11-02 17:14:51 +0200

    Fix camlzip and camlp4

--- overlays/cross-fixes.nix
+++ overlays/cross-fixes.nix
@@ -7,8 +7,20 @@
 in
   liftOCamlOverlay' ({
     osuper,
+    oself,
     ...
   }: {
+    camlzip = osuper.camlzip.overrideAttrs (_: {
+      postInstall = ''
+        ln -sfn $OCAMLFIND_DESTDIR/{,caml}zip
+      '';
+    });
+    camlp4 = osuper.camlp4.overrideAttrs (_: {
+      configurePlatforms = [];
+    });
+    dune-configurator = osuper.dune-configurator.overrideAttrs (_o: {
+      preBuild = "rm -rf vendor/csexp vendor/pp";
+    });
     pyml = osuper.pyml.override {utop = null;};
     ocaml_pcre = osuper.ocaml_pcre.overrideAttrs (_o: {
       nativeBuildInputs = [osuper.dune-configurator];
